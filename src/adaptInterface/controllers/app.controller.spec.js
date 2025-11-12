c2211294625fae51c30b344fd69e08ec599aa08a
elLucas12 <l.zunho@edu.pucrs.br>
2025-11-12 00:23:25 -0300

    Ajuste do Jest & Teste unitário do AppController

--- src/adaptInterface/controllers/app.controller.spec.js
+++ src/adaptInterface/controllers/app.controller.spec.js
@@ -1,21 +1,126 @@
 import { Test } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { AppModule } from './app.module';
 
-// describe('AppController', () => {
-//   let appController;
+describe('AppController', () => {
+  let appController;
+  let ids;
+  let app;
+  jest.setTimeout(30000);
 
-//   beforeEach(async () => {
-//     const app = await Test.createTestingModule({
-//       controllers: [AppController],
-//       providers: [AppService],
-//     }).compile();
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
 
-//     appController = app.get(AppController);
-//   });
+    appController = app.get(AppController);
+    ids = {
+      escala: 1,
+      setor: 1,
+      funcao: 1,
+      funcionario: 1
+    };
+  });
 
-//   describe('root', () => {
-//     it('should return "Hello World!"', () => {
-//       expect(appController.getHello()).toBe('Hello World!');
-//     });
-//   });
-// });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Lógica Op. cadastrar funcionário', () => {
+    const escala = {
+      nome: "Escala 5x1 (Gerente)",
+      horasDiarias: 8,
+      diasSemana: 5
+    };
+    const setor = {
+      nome: "Administração",
+      descricao: "Administra a organização",
+      descricaoAtividades: "Tarefas administrativas e tomadas de decisões em geral."
+    };
+    let funcao = {
+      nome: "Gerente",
+      salario: 10500.58,
+      // setor: 1,
+      // escala: 1
+    };
+    let funcionario = {
+      nome: "Nicole",
+      cpf: "12345678901",
+      ctps: "12345/rj",
+      rg: "123456789",
+      email: "nicole.123@email.com",
+      telefone: "21987654321",
+      endereco: "Rua das Flores, 123 - Copacabana, RJ",
+      // funcao: 1,
+      dataAdmissao: "2025-11-09",
+      dataDemissao: null,
+      flag: 0
+    };
+
+    // Registrando as entidades necessárias para criação do funcionário no sistema.
+    it('Registrar escala', async() => {
+      const resp = await appController.postRegistrarEscala(escala);
+      expect(resp.nome).toBe(escala.nome);
+      expect(resp.horasDiarias).toBe(escala.horasDiarias);
+      expect(resp.diasSemana).toBe(escala.diasSemana);
+      ids.escala = resp.id;
+    });
+    it('Registrar setor', async() => {
+      const resp = await appController.postRegistrarSetor(setor);
+      expect(resp.nome).toBe(setor.nome);
+      expect(resp.descricao).toBe(setor.descricao);
+      // expect(resp.descricaoAtividades).toBe(setor.descricaoAtividades);
+      ids.setor = resp.id;
+    });
+    it('Registrar função', async() => {
+      funcao.setor = ids.setor;
+      funcao.escala = ids.escala;
+      const resp = await appController.postRegistrarFuncao(funcao);
+      expect(resp.nome).toBe(funcao.nome);
+      expect(resp.salario).toBe(funcao.salario);
+      expect(resp.setor).toBe(ids.setor);
+      expect(resp.escala).toBe(ids.escala);
+      ids.funcao = resp.id;
+    });
+    it('Registrar funcionário', async() => {
+      funcionario.funcao = ids.funcao;
+      const resp = await appController.postCadastrarFuncionario(funcionario);
+      expect(resp.nome).toBe(funcionario.nome);
+      expect(resp.cpf).toBe(funcionario.cpf);
+      expect(resp.ctps).toBe(funcionario.ctps);
+      expect(resp.rg).toBe(funcionario.rg);
+      expect(resp.email).toBe(funcionario.email);
+      expect(resp.telefone).toBe(funcionario.telefone);
+      expect(resp.endereco).toBe(funcionario.endereco);
+      expect(resp.funcao).toBe(ids.funcao);
+      expect(resp.dataAdmissao).toBe(funcionario.dataAdmissao);
+      expect(resp.dataDemissao).toBe(funcionario.dataDemissao);
+      expect(resp.flag).toBe(funcionario.flag);
+      ids.funcionario = resp.id;
+    });
+
+    // Consultando a instância da entidade criada no sistema.
+    it('Consultar funcionário', async () => {
+      const resp = await appController.getFuncionarioPorId(ids.funcionario);
+      expect(resp.nome).toBe(funcionario.nome);
+      expect(resp.cpf).toBe(funcionario.cpf);
+      expect(resp.ctps).toBe(funcionario.ctps);
+      expect(resp.rg).toBe(funcionario.rg);
+      expect(resp.email).toBe(funcionario.email);
+      expect(resp.telefone).toBe(funcionario.telefone);
+      expect(resp.endereco).toBe(funcionario.endereco);
+      // expect(resp.funcao).toBe(ids.funcao); // É recebido um dado estruturado!
+      expect(new Date(resp.dataAdmissao)).toStrictEqual(new Date(funcionario.dataAdmissao));
+      expect(new Date(resp.dataDemissao)).toStrictEqual(new Date(funcionario.dataDemissao));
+      expect(resp.flag).toBe(funcionario.flag);
+    });
+  });
+  describe('Deletar Entidades de Teste', () => {
+    it('Deletar entidades', async() => {
+      await appController.deleteFuncionarioPorId(ids.funcionario);
+      await appController.deleteFuncaoPorId(ids.funcao);
+      await appController.deleteSetorPorId(ids.setor);
+      await appController.deleteEscalaPorId(ids.escala);
+    })
+  });
+});
--- test/app.e2e-spec.js
+++ test/app.e2e-spec.js
@@ -14,34 +14,101 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/funcionarios/registrar (POST)', async () => {
-    const funcionarioItem = {
+  decribe('Registrar funcionário', () => {
+    const escala = {
+      nome: "Escala 6x1",
+      horasDiarias: 8,
+      diasSemana: 6
+    };
+    const setor = {
+      nome: "Estoque",
+      descricao: "Estoque de produtos",
+      descricaoAtividades: "Recebimento, armazenamento e separação de produtos."
+    };
+    const funcao = {
+      nome: "Estoquista",
+      salario: 1800.32,
+      setor: 1,
+      escala: 1
+    };
+    const funcionario = {
       nome: "Ana",
       cpf: "12345678901",
       ctps: "12345/rj",
       rg: "123456789",
       email: "ana.123@email.com",
       telefone: "21987654321",
-      endereco: "Rua das Flores, 123 - Copacabana, RJ"
+      endereco: "Rua das Flores, 123 - Copacabana, RJ",
+      funcao: 1,
+      dataAdmissao: "2025-11-09 00:00:00",
+      dataDemissao: null,
+      flag: 0
     };
-
-    const resp = await request(app)
-      .post('/funcionarios/registrar')
-      .send(funcionarioItem)
-      .set('Accept', 'application/json');
-    
-    expect(resp.statusCode).toBe(201);
-    expect(resp.body.nome).toBe(funcionarioItem.nome);
-    expect(resp.body.cpf).toBe(funcionarioItem.cpf);
-    expect(resp.body.ctps).toBe(funcionarioItem.ctps);
-    expect(resp.body.rg).toBe(funcionarioItem.rg);
-    expect(resp.body.email).toBe(funcionarioItem.email);
-    expect(resp.body.telefone).toBe(funcionarioItem.telefone);
-    expect(resp.body.endereco).toBe(funcionarioItem.endereco);
-    expect(resp.headers['content-type']).toMatch(/json/);
+    it('/escalas/registrar (POST)', async() => {
+      const resp = await request(app)
+        .post('/escalas/registrar')
+        .send(escala)
+        .set('Accept', 'application/json');
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body.nome).toBe(escala.nome);
+      expect(resp.body.horasDiarias).toBe(escala.horasDiarias);
+      expect(resp.body.diasSemana).toBe(escala.diasSemana);
+      expect(resp.headers['content-type']).toMatch(/json/);
+    });
+    it('/setores/registrar (POST)', async() => {
+      const resp = await request(app)
+        .post('/setores/registrar')
+        .send(setor)
+        .set('Accept', 'application/json');
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body.nome).toBe(setor.nome);
+      expect(resp.body.descricao).toBe(setor.descricao);
+      expect(resp.body.descricaoAtividades).toBe(setor.descricaoAtividades);
+      expect(resp.headers['content-type']).toMatch(/json/);
+    });
+    it('/funcoes/registrar (POST)', async() => {
+      const resp = await request(app)
+        .post('/funcoes/registrar')
+        .send(setor)
+        .set('Accept', 'application/json');
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body.nome).toBe(funcao.nome);
+      expect(resp.body.salario).toBe(funcao.salario);
+      expect(resp.body.setor).toBe(funcao.setor);
+      expect(resp.body.escala).toBe(funcao.escala);
+      expect(resp.headers['content-type']).toMatch(/json/);
+    });
+    it('/funcionarios/registrar (POST)', async() => {
+      const resp = await request(app)
+        .post('/funcionarios/registrar')
+        .send(funcionario)
+        .set('Accept', 'application/json');
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body.nome).toBe(funcionario.nome);
+      expect(resp.body.cpf).toBe(funcionario.cpf);
+      expect(resp.body.ctps).toBe(funcionario.ctps);
+      expect(resp.body.rg).toBe(funcionario.rg);
+      expect(resp.body.email).toBe(funcionario.email);
+      expect(resp.body.telefone).toBe(funcionario.telefone);
+      expect(resp.body.endereco).toBe(funcionario.endereco);
+      expect(resp.body.funcao).toBe(funcionario.funcao);
+      expect(resp.body.dataAdmissao).toBe(funcionario.dataAdmissao);
+      expect(resp.body.dataDemissao).toBe(funcionario.dataDemissao);
+      expect(resp.body.flag).toBe(funcionario.flag);
+      expect(resp.headers['content-type']).toMatch(/json/);
+    });
   });
 
-  it('Ret. requisição inválida (400) /funcionarios/registrar (POST)', async () => {
+  describe('Deletar funcionário', () => {
+    it('/funcionarios/:id (id=1)', async() => {
+      const resp = await request(app)
+        .delete('/funcionarios/1')
+        .set('Accept', 'application/json');
+      expect(resp.statusCode).toBe(200);
+    });
+  });
+
+  it('Ret. requisição inválida (400) /funcionarios/registrar (POST)', async() => {
     const funcionarioInvalidoItem = {
       name: "item invalido",
       invalidez: "inválido"
@@ -54,11 +121,4 @@ describe('AppController (e2e)', () => {
     
     expect(resp.statusCode).toBe(400);
   });
-
-  // it('/ (GET)', () => {
-  //   return request(app.getHttpServer())
-  //     .get('/funcionarios')
-  //     .expect(200)
-  //     .expect('Hello World!');
-  // });
 });
